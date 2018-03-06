02359309125a5da4ec0a80ac86885e4fa74ad4db
Przemyslaw Malinowski <przemyslaw.malinowski@linuxpolska.pl>
2018-03-06 12:58:32 +0100

    add index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+#$ip = getenv('HTTP_CLIENT_IP')?:
+##echo $ip;
+#echo '</br>';
+##echo $_SERVER;
+##echo $_SERVER['SERVER_NAME'].'</br>';
+##echo $_SERVER['REMOTE_ADDR'].'</br>';
+#echo $_SERVER['SERVER_ADDR'].'</br>';
+##echo $_SERVER['SERVER_SOFTWARE'].'</br>';
+##echo $_SERVER['SERVER_NAME'].'</br>';
+#?>
