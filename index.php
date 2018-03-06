743caaf768bbab4fe6639d39682e87052e09707f
Przemyslaw Malinowski <przemyslaw.malinowski@linuxpolska.pl>
2018-03-06 15:09:52 +0100

    change index.php v16

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 echo '<html>';
 echo '<head></head>';
 echo '<body>';
-echo '<p style="font-size:3em; color:red; margin:0; position:absolute; top:50%; left:50%; margin-right:-50%; width:600px">'; 
+echo '<p style="font-size:3em; color:red; margin:0; position:absolute; top:50%; left:50%; margin-left:-175px; width:350px">'; 
 echo $_SERVER['SERVER_ADDR'].'</br>';
 ##echo $_SERVER['SERVER_SOFTWARE'].'</br>'; 
 echo '</p>';
