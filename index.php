a614011b3b5855bb80f44ed80881b2b7e359adce
Przemyslaw Malinowski <przemyslaw.malinowski@linuxpolska.pl>
2018-03-06 14:47:13 +0100

    change index.php v6

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 ##echo $_SERVER;
 ##echo $_SERVER['SERVER_NAME'].'</br>';
 ##echo $_SERVER['REMOTE_ADDR'].'</br>';
-echo '<p style=\\"font-size:46px; color:red; margin:0; position:absolute; top:50%; left:50%; margin-right:-50%; transform: translate(-50%, -50%):\\">'; 
+echo '<div style=\\"font-size:46px; color:red; margin:0; position:absolute; top:50%; left:50%; margin-right:-50%; transform: translate(-50%, -50%):\\">'; 
 echo $_SERVER['SERVER_ADDR'].'</br>';
 echo $_SERVER['SERVER_SOFTWARE'].'</br>'; 
-echo '</p>';
+echo '</div>';
 ##echo $_SERVER['SERVER_NAME'].'</br>';
 ?>
