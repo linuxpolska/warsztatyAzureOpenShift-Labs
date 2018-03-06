bc4a86cf81838619abae51f0efc4efe14ff2e5fa
Przemyslaw Malinowski <przemyslaw.malinowski@linuxpolska.pl>
2018-03-06 13:13:06 +0100

    change index.php v3

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 ##echo $_SERVER;
 echo $_SERVER['SERVER_NAME'].'</br>';
 ##echo $_SERVER['REMOTE_ADDR'].'</br>';
-echo $_SERVER['SERVER_ADDR'].'</br>';
+##echo $_SERVER['SERVER_ADDR'].'</br>';
 echo $_SERVER['SERVER_SOFTWARE'].'</br>';
-echo $_SERVER['SERVER_NAME'].'</br>';
+##echo $_SERVER['SERVER_NAME'].'</br>';
 #
 ?>
