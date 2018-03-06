cdf8f2682d88f9571c9bf62997888bd2f4671c63
Przemyslaw Malinowski <przemyslaw.malinowski@linuxpolska.pl>
2018-03-06 14:55:27 +0100

    change index.php v9

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 echo '<html>';
 echo '<head></head>';
 echo '<body>';
-echo '<div style=\\"font-size:46px; color:red; margin:0; position:absolute; top:50%; left:50%; margin-right:-50%; transform: translate(-50%, -50%):\\">'; 
+echo '<div style=\\"font-size\\:46px\\; color\\:red\\; margin\\:0\\; position\\:absolute\\; top\\:50%\\; left\\:50%\\; margin-right\\:-50%\\; transform\\: translate(-50%, -50%)\\:\\">'; 
 echo $_SERVER['SERVER_ADDR'].'</br>';
 echo $_SERVER['SERVER_SOFTWARE'].'</br>'; 
 echo '</div>';
